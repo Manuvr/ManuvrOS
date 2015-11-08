090d624ad93b56ca9765cc2e848e2d1f3f4a2a74
jspark311 <josh.lindsay@gmail.com>
2015-11-08 15:10:15 -0700

    Preparing to add STM32F7 support to the i2c adapter.

--- ManuvrOS/Drivers/i2c-adapter/i2c-adapter.cpp
+++ ManuvrOS/Drivers/i2c-adapter/i2c-adapter.cpp
@@ -55,6 +55,97 @@ void I2CAdapter::__class_initializer() {
 
 #ifdef STM32F4XX
 
+I2CAdapter::I2CAdapter(uint8_t dev_id) {
+  __class_initializer();
+  dev = dev_id;
+
+  // This init() fxn was patterned after the STM32F4x7 library example.
+  if (dev_id == 1) {
+    //I2C_DeInit(I2C1);		//Deinit and reset the I2C to avoid it locking up
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);    // enable APB1 peripheral clock for I2C1
+
+    RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1, ENABLE);
+    RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1, DISABLE);
+    I2C_DeInit(I2C1);
+
+    /* Reset I2Cx IP */
+    
+    /* Release reset signal of I2Cx IP */
+    //RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C1, DISABLE);
+    
+    /* setup SCL and SDA pins
+     * You can connect the I2C1 functions to two different
+     * pins:
+     * 1. SCL on PB6
+     * 2. SDA on PB7
+     */
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // we are going to use PB6 and PB7
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;           // set pins to alternate function
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;      // set GPIO speed
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;         // set output to open drain --> the line has to be only pulled low, not driven high
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;       // enable pull up resistors
+    GPIO_Init(GPIOB, &GPIO_InitStruct);                 // init GPIOB
+    
+    // Connect I2C1 pins to AF
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_I2C1);    // SCL
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_I2C1);    // SDA
+    
+    I2C_InitTypeDef I2C_InitStruct;
+
+   
+    // configure I2C1 
+    I2C_InitStruct.I2C_ClockSpeed = 400000;          // 400kHz
+    I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;          // I2C mode
+    I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;  // 50% duty cycle --> standard
+    I2C_InitStruct.I2C_OwnAddress1 = 0x00;           // own address, not relevant in master mode
+    //I2C_InitStruct.I2C_Ack = I2C_Ack_Disable;         // disable acknowledge when reading (can be changed later on)
+    I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit; // set address length to 7 bit addresses
+    I2C_Init(I2C1, &I2C_InitStruct);                 // init I2C1
+    I2C_Cmd(I2C1, ENABLE);       // enable I2C1
+  }
+  
+  for (uint16_t i = 0; i < 128; i++) {
+  	  ping_map[i] = 0;
+  }
+}
+
+
+I2CAdapter::~I2CAdapter() {
+    I2C_ITConfig(I2C1, I2C_IT_EVT|I2C_IT_ERR, DISABLE);   // Shelve the interrupts.
+    I2C_DeInit(I2C1);   // De-init
+    while (dev_list.hasNext()) {
+      dev_list.get()->disassignBusInstance();
+      dev_list.remove();
+    }
+    
+    /* TODO: The work_queue destructor will take care of its own cleanup, but
+       We should abort any open transfers prior to deleting this list. */
+}
+
+
+// TODO: Inline this.
+int8_t I2CAdapter::generateStart() {
+  if (verbosity > 6) StaticHub::log("I2CAdapter::generateStart()\n");
+  if (! bus_online) return -1;
+  I2C_ITConfig(I2C1, I2C_IT_EVT|I2C_IT_ERR, ENABLE);      //Enable EVT and ERR interrupts
+  I2C_GenerateSTART(I2C1, ENABLE);   // Doing this will take us back to the ISR.
+  return 0;
+}
+
+// TODO: Inline this.
+int8_t I2CAdapter::generateStop() {
+  if (verbosity > 6) StaticHub::log("I2CAdapter::generateStop()\n");
+  if (! bus_online) return -1;
+  DMA_ITConfig(DMA1_Stream0, DMA_IT_TC | DMA_IT_TE | DMA_IT_FE | DMA_IT_DME, DISABLE);
+  I2C_ITConfig(I2C1, I2C_IT_EVT|I2C_IT_ERR, DISABLE);
+  I2C_GenerateSTOP(I2C1, ENABLE);
+  return 0;
+}
+
+#elif defined(STM32F7XX) // STM32F7?
 
 I2CAdapter::I2CAdapter(uint8_t dev_id) {
   __class_initializer();
@@ -147,7 +238,7 @@ int8_t I2CAdapter::generateStop() {
 }
 
 
-#elif defined(__MK20DX256__) | defined(__MK20DX128__)
+#elif defined(__MK20DX256__) | defined(__MK20DX128__)  // Teensyduino? 3.x?
 
 I2CAdapter::I2CAdapter(uint8_t dev_id) {
   __class_initializer();
@@ -203,7 +294,7 @@ int8_t I2CAdapter::generateStop() {
 }
 
 
-#elif defined(_BOARD_FUBARINO_MINI_)    // Perhaps this is an arduino-style env?
+#elif defined(_BOARD_FUBARINO_MINI_)    // Perhaps this is an arduino-style env on a Fubarino?
 
 
 I2CAdapter::I2CAdapter(uint8_t dev_id) {
--- ManuvrOS/Platform/Platform.cpp
+++ ManuvrOS/Platform/Platform.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "Platform.h"
 
+
 #include <sys/time.h>
 #include <unistd.h>
 
