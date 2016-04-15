8ea259df9e80f9a77de5d8740abe858d54233150
jspark311 <josh.lindsay@gmail.com>
2016-04-15 15:29:52 -0700

    Fixing dep-retrieval script.

--- downloadDeps.sh
+++ downloadDeps.sh
@@ -6,22 +6,21 @@
 
 # Make the lib directory...
 mkdir lib
-cd lib
 
 # JSON support via ArduinoJson...
-git clone https://github.com/bblanchon/ArduinoJson.git lib/ArduinoJson
+git clone https://github.com/bblanchon/ArduinoJson.git ArduinoJson
 
 # FreeRTOS...
-git clone https://github.com/greiman/FreeRTOS-Arduino lib/FreeRTOS_Arduino
+git clone https://github.com/greiman/FreeRTOS-Arduino FreeRTOS_Arduino
 
 # CoAP, if desired.
-git clone https://github.com/eclipse/wakaama.git lib/wakaama
+git clone https://github.com/eclipse/wakaama.git wakaama
 
 # MQTT, if desired.
-git clone https://github.com/eclipse/paho.mqtt.embedded-c.git lib/paho.mqtt.embedded-c
+git clone https://github.com/eclipse/paho.mqtt.embedded-c.git paho.mqtt.embedded-c
 
 # Telehash
-git clone https://github.com/telehash/telehash-c.git lib/
+git clone https://github.com/telehash/telehash-c.git lib/telehash-c
 
 # Arduino libraries...
 
