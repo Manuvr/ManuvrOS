e2ff3dffc182ac8ca25a4c07da8fd17db3991bd9
jspark311 <josh.lindsay@gmail.com>
2016-05-26 15:52:00 -0700

    Decided on a strategy for GPIO port abstraction.

---
+++ ManuvrOS/Platform/STM32F7.h
@@ -0,0 +1,52 @@
+/*
+File:   STM32F7.h
+Author: J. Ian Lindsay
+Date:   2016.05.26
+
+Copyright 2016 Manuvr, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+#ifndef __PLATFORM_STM32F7_H__
+#define __PLATFORM_STM32F7_H__
+
+#include "stm32f7xx_hal.h"
+#include "stm32f7xx_hal_rcc.h"
+#include "stm32f7xx_hal_gpio.h"
+#include "stm32f7xx_hal_gpio_ex.h"
+
+#define PLATFORM_GPIO_PIN_COUNT   48
+
+/*
+* For the STM32 parts, we will treat pins as being the linear sequence of ports
+*   and pins. This incurs a slight performance hit as we need to shift and divide
+*   each time we call a pin by its 8-bit identifier. But this is the cost of
+*   uniformity across platforms.
+*/
+GPIO_TypeDef* _port_assoc[] = {
+  GPIOA,  GPIOB,  GPIOC,  GPIOD,  GPIOE,
+  GPIOF,  GPIOG,  GPIOH,  GPIOI,  GPIOJ,
+  GPIOK
+};
+
+inline GPIO_TypeDef* _associated_port(uint8_t _pin) {
+  return _port_assoc[_pin >> 4];
+}
+
+inline uint16_t _associated_pin(uint8_t _pin) {
+  return (1 << (_pin % 16));
+}
+
+#endif  // __PLATFORM_STM32F7_H__
