67796547d8021ca5803016f274a465ceff5d581a
jspark311 <josh.lindsay@gmail.com>
2016-09-26 10:59:35 -0700

    Putting Arduino support back on track ahead of MKR1000.

---
+++ ManuvrOS/Platform/AppleOSX/AppleOSX.h
@@ -0,0 +1,81 @@
+/*
+File:   AppleOSX.h
+Author: J. Ian Lindsay
+Date:   2016.09.23
+
+Copyright 2016 Manuvr, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+This file is meant to contain a set of common functions that are
+  typically platform-dependent. The goal is to make a class instance
+  that is pre-processor-selectable to reflect the platform with an API
+  that is consistent, thereby giving the kernel the ability to...
+    * Access the realtime clock (if applicatble)
+    * Get definitions for GPIO pins.
+    * Access a true RNG (if it exists)
+    * Persist and retrieve data across runtimes.
+*/
+
+
+#ifndef __PLATFORM_APPLE_OSX_H__
+#define __PLATFORM_APPLE_OSX_H__
+#include <pthread.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#if defined(MANUVR_STORAGE)
+#include <Platform/Linux/LinuxStorage.h>
+#endif
+
+#if !defined(__MACH__) || !defined(__APPLE__)
+  #error Presently, only El Capitan and above are supported.
+#endif
+
+
+/* Used to build an Argument chain from parameters passed to main(). */
+Argument* parseFromArgCV(int argc, const char* argv[]);
+
+
+class ApplePlatform : public ManuvrPlatform {
+  public:
+    inline  int8_t platformPreInit() {   return platformPreInit(nullptr); };
+    virtual int8_t platformPreInit(Argument*);
+    virtual void   printDebug(StringBuilder* out);
+
+    /* Platform state-reset functions. */
+    virtual void seppuku();           // Simple process termination. Reboots if not implemented.
+    virtual void reboot();
+    virtual void hardwareShutdown();
+    virtual void jumpToBootloader();
+
+
+  protected:
+    const char* _board_name = "Generic";
+    virtual int8_t platformPostInit();
+
+    #if defined(MANUVR_STORAGE)
+      // Called during boot to load configuration.
+      int8_t _load_config();
+    #endif
+
+
+  private:
+    uint8_t _binary_hash[32];
+
+    int8_t internal_integrity_check(uint8_t* test_buf, int test_len);
+    int8_t hash_self();
+};
+
+#endif  // __PLATFORM_APPLE_OSX_H__
