53a905f4f03a3e433ed7b3ed4a71ae2455f4b0ff
jspark311 <josh.lindsay@gmail.com>
2016-10-13 15:47:54 -0700

    Transfer commit.

--- ManuvrOS/Platform/Cryptographic/Cryptographic.c
+++ ManuvrOS/Platform/Cryptographic/Cryptographic.c
@@ -152,66 +152,160 @@ bool estimate_pk_size_requirements(CryptoKey k, size_t* pub, size_t* priv, uint1
 * Parameter compatibility checking matricies...                                *
 *******************************************************************************/
 /* Privately scoped. */
+#if defined(__BUILD_HAS_SYMMETRIC) || defined(__BUILD_HAS_ASYMMETRIC)
 const bool _is_cipher_symmetric(Cipher ci) {
   switch (ci) {
     #if defined(WRAPPED_SYM_NULL)
-      case Cipher::SYM_NULL:
+      case Cipher::SYM_NULL:             return true;
     #endif
-    case Cipher::SYM_AES_128_ECB:
-    case Cipher::SYM_AES_192_ECB:
-    case Cipher::SYM_AES_256_ECB:
-    case Cipher::SYM_AES_128_CBC:
-    case Cipher::SYM_AES_192_CBC:
-    case Cipher::SYM_AES_256_CBC:
-    case Cipher::SYM_AES_128_CFB128:
-    case Cipher::SYM_AES_192_CFB128:
-    case Cipher::SYM_AES_256_CFB128:
-    case Cipher::SYM_AES_128_CTR:
-    case Cipher::SYM_AES_192_CTR:
-    case Cipher::SYM_AES_256_CTR:
-    case Cipher::SYM_AES_128_GCM:
-    case Cipher::SYM_AES_192_GCM:
-    case Cipher::SYM_AES_256_GCM:
-    case Cipher::SYM_AES_128_CCM:
-    case Cipher::SYM_AES_192_CCM:
-    case Cipher::SYM_AES_256_CCM:
-    case Cipher::SYM_CAMELLIA_128_ECB:
-    case Cipher::SYM_CAMELLIA_192_ECB:
-    case Cipher::SYM_CAMELLIA_256_ECB:
-    case Cipher::SYM_CAMELLIA_128_CBC:
-    case Cipher::SYM_CAMELLIA_192_CBC:
-    case Cipher::SYM_CAMELLIA_256_CBC:
-    case Cipher::SYM_CAMELLIA_128_CFB128:
-    case Cipher::SYM_CAMELLIA_192_CFB128:
-    case Cipher::SYM_CAMELLIA_256_CFB128:
-    case Cipher::SYM_CAMELLIA_128_CTR:
-    case Cipher::SYM_CAMELLIA_192_CTR:
-    case Cipher::SYM_CAMELLIA_256_CTR:
-    case Cipher::SYM_CAMELLIA_128_GCM:
-    case Cipher::SYM_CAMELLIA_192_GCM:
-    case Cipher::SYM_CAMELLIA_256_GCM:
-    //case Cipher::SYM_DES_ECB:
-    //case Cipher::SYM_DES_CBC:
-    //case Cipher::SYM_DES_EDE_ECB:
-    //case Cipher::SYM_DES_EDE_CBC:
-    //case Cipher::SYM_DES_EDE3_ECB:
-    //case Cipher::SYM_DES_EDE3_CBC:
-    case Cipher::SYM_BLOWFISH_ECB:
-    case Cipher::SYM_BLOWFISH_CBC:
-    case Cipher::SYM_BLOWFISH_CFB64:
-    case Cipher::SYM_BLOWFISH_CTR:
+    #if defined(WRAPPED_SYM_AES_128_GCM)
+      case Cipher::SYM_AES_128_GCM:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_192_GCM)
+      case Cipher::SYM_AES_192_GCM:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_256_GCM)
+      case Cipher::SYM_AES_256_GCM:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_128_CCM)
+      case Cipher::SYM_AES_128_CCM:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_192_CCM)
+      case Cipher::SYM_AES_192_CCM:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_256_CCM)
+      case Cipher::SYM_AES_256_CCM:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_128_ECB)
+      case Cipher::SYM_AES_128_ECB:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_192_ECB)
+      case Cipher::SYM_AES_192_ECB:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_256_ECB)
+      case Cipher::SYM_AES_256_ECB:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_128_CBC)
+      case Cipher::SYM_AES_128_CBC:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_192_CBC)
+      case Cipher::SYM_AES_192_CBC:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_256_CBC)
+      case Cipher::SYM_AES_256_CBC:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_128_CFB128)
+      case Cipher::SYM_AES_128_CFB128:   return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_192_CFB128)
+      case Cipher::SYM_AES_192_CFB128:   return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_256_CFB128)
+      case Cipher::SYM_AES_256_CFB128:   return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_128_CTR)
+      case Cipher::SYM_AES_128_CTR:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_192_CTR)
+      case Cipher::SYM_AES_192_CTR:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_AES_256_CTR)
+      case Cipher::SYM_AES_256_CTR:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_128_GCM)
+      case Cipher::SYM_CAMELLIA_128_GCM: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_192_GCM)
+      case Cipher::SYM_CAMELLIA_192_GCM: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_256_GCM)
+      case Cipher::SYM_CAMELLIA_256_GCM: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_128_CCM)
+      case Cipher::SYM_CAMELLIA_128_CCM: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_192_CCM)
+      case Cipher::SYM_CAMELLIA_192_CCM: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_256_CCM)
+      case Cipher::SYM_CAMELLIA_256_CCM: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_128_ECB)
+      case Cipher::SYM_CAMELLIA_128_ECB: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_192_ECB)
+      case Cipher::SYM_CAMELLIA_192_ECB: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_256_ECB)
+      case Cipher::SYM_CAMELLIA_256_ECB: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_128_CBC)
+      case Cipher::SYM_CAMELLIA_128_CBC: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_192_CBC)
+      case Cipher::SYM_CAMELLIA_192_CBC: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_256_CBC)
+      case Cipher::SYM_CAMELLIA_256_CBC: return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_128_CFB128)
+      case Cipher::SYM_CAMELLIA_128_CFB128:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_192_CFB128)
+      case Cipher::SYM_CAMELLIA_192_CFB128:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_256_CFB128)
+      case Cipher::SYM_CAMELLIA_256_CFB128:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_128_CTR)
+      case Cipher::SYM_CAMELLIA_128_CTR:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_192_CTR)
+      case Cipher::SYM_CAMELLIA_192_CTR:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_CAMELLIA_256_CTR)
+      case Cipher::SYM_CAMELLIA_256_CTR:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_DES_ECB)
+      case Cipher::SYM_DES_ECB:          return true;
+    #endif
+    #if defined(WRAPPED_SYM_DES_CBC)
+      case Cipher::SYM_DES_CBC:          return true;
+    #endif
+    #if defined(WRAPPED_SYM_DES_EDE_ECB)
+      case Cipher::SYM_DES_EDE_ECB:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_DES_EDE_CBC)
+      case Cipher::SYM_DES_EDE_CBC:      return true;
+    #endif
+    #if defined(WRAPPED_SYM_DES_EDE3_ECB)
+      case Cipher::SYM_DES_EDE3_ECB:     return true;
+    #endif
+    #if defined(WRAPPED_SYM_DES_EDE3_CBC)
+      case Cipher::SYM_DES_EDE3_CBC:     return true;
+    #endif
+    #if defined(WRAPPED_SYM_BLOWFISH_ECB)
+      case Cipher::SYM_BLOWFISH_ECB:     return true;
+    #endif
+    #if defined(WRAPPED_SYM_BLOWFISH_CBC)
+      case Cipher::SYM_BLOWFISH_CBC:     return true;
+    #endif
+    #if defined(WRAPPED_SYM_BLOWFISH_CFB64)
+      case Cipher::SYM_BLOWFISH_CFB64:   return true;
+    #endif
+    #if defined(WRAPPED_SYM_BLOWFISH_CTR)
+      case Cipher::SYM_BLOWFISH_CTR:     return true;
+    #endif
     #if defined(WRAPPED_SYM_ARC4_128)
-      case Cipher::SYM_ARC4_128:
+      case Cipher::SYM_ARC4_128:         return true;
     #endif
-    case Cipher::SYM_CAMELLIA_128_CCM:
-    case Cipher::SYM_CAMELLIA_192_CCM:
-    case Cipher::SYM_CAMELLIA_256_CCM:
-    case Cipher::SYM_NONE:
-      return true;
-    default:
-      return false;
+    #if defined(WRAPPED_SYM_NONE)
+      case Cipher::SYM_NONE:             return true;
+    #endif
+    default:  return false;
   }
 }
+#endif
 
 /* Privately scoped. */
 const bool _is_cipher_authenticated(Cipher ci) {
@@ -317,83 +411,105 @@ const bool _valid_cipher_params(Cipher ci) {
 /**
 * Tests for an implementation-specific deferral for the given cipher.
 *
-* @param Cipher The cipher to test for deferral.
+* @param enum Cipher The cipher to test for deferral.
 * @return true if the root function ought to defer.
 */
-bool cipher_deferred_handling(Cipher ci) {
+bool cipher_deferred_handling(enum Cipher ci) {
+  #if defined(WITH_BLIND_CRYPTO)
   // TODO: Slow. Ugly.
-  return (_sym_overrides[ci] || _sauth_overrides[ci]);
+  return (_sym_overrides[ci] || _sauth_overrides[ci] || _asym_overrides[ci]);
+  #else
+  return false;
+  #endif
 }
 
 /**
 * Tests for an implementation-specific deferral for the given hash.
 *
-* @param Hashes The hash to test for deferral.
+* @param enum Hashes The hash to test for deferral.
 * @return true if the root function ought to defer.
 */
-bool digest_deferred_handling(Hashes h) {
+bool digest_deferred_handling(enum Hashes h) {
+  #if defined(WITH_BLIND_CRYPTO)
   return ((bool)(_hash_overrides[h]));
+  #else
+  return false;
+  #endif
 }
 
 
 /**
 * Tests for an implementation-specific deferral for the given sign/verify algorithm.
 *
-* @param CryptoKey The key type to test for deferral.
+* @param enum CryptoKey The key type to test for deferral.
 * @return true if the root function ought to defer.
 */
-bool sign_verify_deferred_handling(CryptoKey k) {
+bool sign_verify_deferred_handling(enum CryptoKey k) {
+  #if defined(WITH_BLIND_CRYPTO)
   return ((bool)(_s_v_overrides[k]));
+  #else
+  return false;
+  #endif
 }
 
 
 /**
 * Tests for an implementation-specific deferral for key generation using the given algorithm.
 *
-* @param CryptoKey The key type to test for deferral.
+* @param enum CryptoKey The key type to test for deferral.
 * @return true if the root function ought to defer.
 */
-bool keygen_deferred_handling(CryptoKey k) {
+bool keygen_deferred_handling(enum CryptoKey k) {
+  #if defined(WITH_BLIND_CRYPTO)
   return ((bool)(_keygen_overrides[k]));
+  #else
+  return false;
+  #endif
 }
 
 
 
-bool provide_cipher_handler(Cipher c, wrapped_sym_operation fxn) {
+bool provide_cipher_handler(enum Cipher c, wrapped_sym_operation fxn) {
+  #if defined(WITH_BLIND_CRYPTO)
   if (!cipher_deferred_handling(c)) {
     _sym_overrides[c] = fxn;
     return true;
   }
+  #endif
   return false;
 }
 
 
-bool provide_digest_handler(Hashes h, wrapped_hash_operation fxn) {
+bool provide_digest_handler(enum Hashes h, wrapped_hash_operation fxn) {
+  #if defined(WITH_BLIND_CRYPTO)
   if (!digest_deferred_handling(h)) {
     _hash_overrides[h] = fxn;
     return true;
   }
+  #endif
   return false;
 }
 
 
-bool provide_sign_verify_handler(CryptoKey k, wrapped_sv_operation fxn) {
+bool provide_sign_verify_handler(enum CryptoKey k, wrapped_sv_operation fxn) {
+  #if defined(WITH_BLIND_CRYPTO)
   if (!sign_verify_deferred_handling(k)) {
     _s_v_overrides[k] = fxn;
     return true;
   }
+  #endif
   return false;
 }
 
 
-bool provide_keygen_handler(CryptoKey k, wrapped_keygen_operation fxn) {
+bool provide_keygen_handler(enum CryptoKey k, wrapped_keygen_operation fxn) {
+  #if defined(WITH_BLIND_CRYPTO)
   if (!keygen_deferred_handling(k)) {
     _keygen_overrides[k] = fxn;
     return true;
   }
+  #endif
   return false;
 }
-
-
 
 #endif  // __HAS_CRYPT_WRAPPER
