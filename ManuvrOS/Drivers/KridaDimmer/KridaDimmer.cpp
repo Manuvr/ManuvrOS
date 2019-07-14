4ed4cee499fc54ab49a99c71d5e965395bf8cef2
jspark311 <josh.lindsay@gmail.com>
2019-07-14 00:58:37 -0600

    Cleanup of timer cruft from KridaDimmer.

--- ManuvrOS/Drivers/KridaDimmer/KridaDimmer.cpp
+++ ManuvrOS/Drivers/KridaDimmer/KridaDimmer.cpp
@@ -21,15 +21,6 @@ limitations under the License.
 #include "KridaDimmer.h"
 
 
-const MessageTypeDef message_defs_dimmer[] = {
-  {  MANUVR_MSG_DIMMER_SET_LEVEL,  MSG_FLAG_EXPORTABLE,  "DIMMER_SET_LEVEL",  ManuvrMsg::MSG_ARGS_NONE }, //
-  {  MANUVR_MSG_DIMMER_TOGGLE,     MSG_FLAG_EXPORTABLE,  "DIMMER_TOGGLE",     ManuvrMsg::MSG_ARGS_NONE }, //
-  {  MANUVR_MSG_DIMMER_FADE_DOWN,  MSG_FLAG_EXPORTABLE,  "DIMMER_FADE_DOWN",  ManuvrMsg::MSG_ARGS_NONE }, //
-  {  MANUVR_MSG_DIMMER_FADE_UP,    MSG_FLAG_EXPORTABLE,  "DIMMER_FADE_UP",    ManuvrMsg::MSG_ARGS_NONE }  //
-};
-
-
-
 /*******************************************************************************
 *   ___ _              ___      _ _              _      _
 *  / __| |__ _ ______ | _ ) ___(_) |___ _ _ _ __| |__ _| |_ ___
@@ -42,17 +33,13 @@ const MessageTypeDef message_defs_dimmer[] = {
 /*
 * Constructor.
 */
-KridaDimmer::KridaDimmer(const KridaDimmerOpts* o) : EventReceiver("KridaDimmer"), I2CDevice(o->dimmer_addr), _opts(o) {
-  int mes_count = sizeof(message_defs_dimmer) / sizeof(MessageTypeDef);
-  ManuvrMsg::registerMessages(message_defs_dimmer, mes_count);
-  memset(_channel_values, 0, 4);
-  memset(_channel_buffer, 0, 4);
-  memset(_channel_hw, 0, 4);
-
+KridaDimmer::KridaDimmer(uint8_t addr) : I2CDevice(addr) {
   for (uint8_t i = 0; i < 4; i++) {
-    _channel_timers[i] = nullptr;
     _channel_minima[i] = 33;
     _channel_maxima[i] = 100;
+    _channel_values[i] = 0;
+    _channel_buffer[i] = 0;
+    _channel_hw[i]     = 0;
   }
 }
 
@@ -61,14 +48,6 @@ KridaDimmer::KridaDimmer(const KridaDimmerOpts* o) : EventReceiver("KridaDimmer"
 * Destructor.
 */
 KridaDimmer::~KridaDimmer() {
-  for (uint8_t i = 0; i < 4; i++) {
-    if (nullptr != _channel_timers[i]) {
-      _channel_timers[i]->enableSchedule(false);
-      _channel_timers[i]->decRefs();
-      platform.kernel()->removeSchedule(_channel_timers[i]);
-      _channel_timers[i] = nullptr;
-    }
-  }
 }
 
 
@@ -133,7 +112,6 @@ int8_t KridaDimmer::io_op_callback(BusOp* _op) {
       break;
   }
 
-  flushLocalLog();
   return 0;
 }
 
@@ -142,317 +120,27 @@ int8_t KridaDimmer::io_op_callback(BusOp* _op) {
 * Dump this item to the dev log.
 */
 void KridaDimmer::printDebug(StringBuilder* output) {
-  output->concat("\n==< KridaDimmer >======================\n\tValue\tHW\tMin\tMax\tTimer\n");
-  uint8_t hw  = 0;
-  uint8_t val = 0;
+  output->concat("\n==< KridaDimmer >======================\n\tValue\tHW\tMin\tMax\n");
   for (uint8_t i = 0; i < 4; i++) {
-    hw  = _channel_hw[i];
-    val = _channel_values[i];
-    output->concatf("\t%u\t%u\t%u\t%u\t%c\n", val, hw, _channel_minima[i], _channel_maxima[i], ((nullptr != _channel_timers[i]) ? 'y' : 'n'));
+    printChannel(i, output);
+    output->concat("\n");
   }
 }
 
 
-
-/*******************************************************************************
-* ######## ##     ## ######## ##    ## ########  ######
-* ##       ##     ## ##       ###   ##    ##    ##    ##
-* ##       ##     ## ##       ####  ##    ##    ##
-* ######   ##     ## ######   ## ## ##    ##     ######
-* ##        ##   ##  ##       ##  ####    ##          ##
-* ##         ## ##   ##       ##   ###    ##    ##    ##
-* ########    ###    ######## ##    ##    ##     ######
-*
-* These are overrides from EventReceiver interface...
-*******************************************************************************/
-
-/**
-* This is called when the kernel attaches the module.
-* This is the first time the class can be expected to have kernel access.
-*
-* @return 0 on no action, 1 on action, -1 on failure.
+/*
+* Dump a channel to the dev log.
 */
-int8_t KridaDimmer::attached() {
-  if (EventReceiver::attached()) {
-    return 1;
+void KridaDimmer::printChannel(uint8_t chan, StringBuilder* output) {
+  if (is_channel_valid(chan)) {
+    output->concatf("\t%u\t%u\t%u\t%u", _channel_values[chan], _channel_hw[chan], _channel_minima[chan], _channel_maxima[chan]);
   }
-  return 0;
 }
-
-
-/**
-* If we find ourselves in this fxn, it means an event that this class built (the argument)
-*   has been serviced and we are now getting the chance to see the results. The argument
-*   to this fxn will never be NULL.
-*
-* Depending on class implementations, we might choose to handle the completed Event differently. We
-*   might add values to event's Argument chain and return RECYCLE. We may also free() the event
-*   ourselves and return DROP. By default, we will return REAP to instruct the Kernel
-*   to either free() the event or return it to it's preallocate queue, as appropriate. If the event
-*   was crafted to not be in the heap in its own allocation, we will return DROP instead.
-*
-* @param  event  The event for which service has been completed.
-* @return A callback return code.
-*/
-int8_t KridaDimmer::callback_proc(ManuvrMsg* event) {
-  /* Setup the default return code. If the event was marked as mem_managed, we return a DROP code.
-     Otherwise, we will return a REAP code. Downstream of this assignment, we might choose differently. */
-  int8_t return_value = (0 == event->refCount()) ? EVENT_CALLBACK_RETURN_REAP : EVENT_CALLBACK_RETURN_DROP;
-
-  /* Some class-specific set of conditionals below this line. */
-  switch (event->eventCode()) {
-    case MANUVR_MSG_DIMMER_TOGGLE:
-    case MANUVR_MSG_DIMMER_FADE_UP:
-    case MANUVR_MSG_DIMMER_FADE_DOWN:
-      if (!event->willRunAgain()) {
-        if (_channel_timers[0] == event) {  _channel_timers[0] = nullptr;   }
-        if (_channel_timers[1] == event) {  _channel_timers[1] = nullptr;   }
-        if (_channel_timers[2] == event) {  _channel_timers[2] = nullptr;   }
-        if (_channel_timers[3] == event) {  _channel_timers[3] = nullptr;   }
-      }
-      break;
-    default:
-      break;
-  }
-
-  return return_value;
-}
-
-
-
-int8_t KridaDimmer::notify(ManuvrMsg* active_event) {
-  int8_t return_value = 0;
-
-  switch (active_event->eventCode()) {
-    case MANUVR_MSG_DIMMER_SET_LEVEL:
-      if (active_event->argCount() == 2) {
-        uint8_t chan;
-        uint8_t val;
-        if (0 == active_event->getArgAs(0, &chan)) {
-          if (0 == active_event->getArgAs(1, &val)) {
-            set_brightness(chan, val);
-            return_value++;
-          }
-        }
-      }
-      break;
-
-    case MANUVR_MSG_DIMMER_TOGGLE:
-      if (active_event->argCount() == 2) {
-        uint8_t chan;
-        uint8_t val;
-        if (0 == active_event->getArgAs(0, &chan)) {
-          if (is_channel_valid(chan)) {
-            if (0 == active_event->getArgAs(1, &val)) {
-              if (channel_enabled(chan)) {
-                enable_channel(chan, false);
-              }
-              else {
-                set_brightness(chan, val);
-              }
-              return_value++;
-            }
-          }
-        }
-      }
-      break;
-
-    case MANUVR_MSG_DIMMER_FADE_DOWN:
-      if (active_event->argCount() == 1) {
-        uint8_t chan;
-        if (0 == active_event->getArgAs(&chan)) {
-          if (is_channel_valid(chan)) {
-            if (!channel_has_outstanding_io(chan)) {
-              if (_channel_minima[chan] < _channel_values[chan]) {
-                set_brightness(chan, _channel_values[chan]-1);
-              }
-              else {
-                // Fade completed. Clean up schedule.
-                enable_channel(chan, false);
-                if (_channel_timers[chan] == active_event) {
-                  active_event->alterScheduleRecurrence(0);
-                  active_event->decRefs();
-                }
-              }
-            }
-            return_value++;
-          }
-        }
-      }
-      break;
-
-    case MANUVR_MSG_DIMMER_FADE_UP:
-      if (active_event->argCount() == 1) {
-        uint8_t chan;
-        if (0 == active_event->getArgAs(&chan)) {
-          if (is_channel_valid(chan)) {
-            if (!channel_has_outstanding_io(chan)) {
-              if (_channel_maxima[chan] > _channel_values[chan]) {
-                set_brightness(chan, channel_enabled(chan) ? _channel_values[chan]+1 : _channel_minima[chan]);
-              }
-              else {
-                // Fade completed. Clean up schedule.
-                if (_channel_timers[chan] == active_event) {
-                  active_event->alterScheduleRecurrence(0);
-                  active_event->decRefs();
-                }
-              }
-            }
-            return_value++;
-          }
-        }
-      }
-      break;
-
-    default:
-      return_value += EventReceiver::notify(active_event);
-      break;
-  }
-
-  flushLocalLog();
-  return return_value;
-}
-
-
-
-#if defined(MANUVR_CONSOLE_SUPPORT)
-/*******************************************************************************
-* Console I/O
-*******************************************************************************/
-
-static const ConsoleCommand console_cmds[] = {
-  { "i", "Info" },
-  { "b", "Set channel brightness" },
-  { "c", "Cycle color temperature" },
-  { "L/l", "(En/Dis)able channel" },
-  { "F/f", "Fade channel (up/down)" },
-  { "i1", "I2CDevice debug" }
-};
-
-
-uint KridaDimmer::consoleGetCmds(ConsoleCommand** ptr) {
-  *ptr = (ConsoleCommand*) &console_cmds[0];
-  return sizeof(console_cmds) / sizeof(ConsoleCommand);
-}
-
-
-void KridaDimmer::consoleCmdProc(StringBuilder* input) {
-  const char* str = (char *) input->position(0);
-  char c    = *str;
-  int channel  = 0;
-  int temp_int = 0;
-
-  if (input->count() > 1) {
-    // If there is a second token, we proceed on good-faith that it's an int.
-    channel = input->position_as_int(1);
-  }
-  if (input->count() > 2) {
-    // If there is a second token, we proceed on good-faith that it's an int.
-    temp_int = input->position_as_int(2);
-  }
-
-  switch (c) {
-    case 'i':   // Debug prints.
-      switch (temp_int) {
-        case 1:   // We want the slot stats.
-          I2CDevice::printDebug(&local_log);
-          break;
-        default:
-          printDebug(&local_log);
-          break;
-      }
-      break;
-    case 'p':
-      pulse_channel(channel, 50, 100, 1);
-      break;
-    case 'l':
-    case 'L':
-      enable_channel(channel, (c == 'L'));
-      break;
-    case 'f':
-    case 'F':
-      fade_channel(channel, (c == 'F'));
-      break;
-    case 'b':
-      set_brightness(channel, temp_int);
-      if (getVerbosity() > 5) local_log.concatf("KridaDimmer: set_brightness(%u, %u)\n", channel, temp_int);
-      break;
-
-    default:
-      break;
-  }
-
-  flushLocalLog();
-}
-#endif  //MANUVR_CONSOLE_SUPPORT
-
 
 
 /*******************************************************************************
 * Dimmer and channel control functions
 *******************************************************************************/
-
-void KridaDimmer::pulse_channel(uint8_t chan, uint8_t nu_brightness, uint32_t period, int16_t recurrence) {
-  switch (chan) {
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-      if (nullptr == _channel_timers[chan]) {
-        _channel_timers[chan] = Kernel::returnEvent(MANUVR_MSG_DIMMER_TOGGLE, this);
-        _channel_timers[chan]->incRefs();
-        _channel_timers[chan]->alterSchedulePeriod(period);
-        _channel_timers[chan]->alterScheduleRecurrence(recurrence);
-        _channel_timers[chan]->autoClear(true);
-        _channel_timers[chan]->enableSchedule(true);
-        _channel_timers[chan]->addArg(chan);
-        _channel_timers[chan]->addArg(nu_brightness);
-        platform.kernel()->addSchedule(_channel_timers[chan]);
-      }
-      else {
-        // There is already a timed operation on the channel.
-        if (getVerbosity() > 4) local_log.concatf("KridaDimmer: Won't pulse channel %u because a timer is already operating on the channel.\n", chan);
-      }
-      break;
-
-    default:
-      if (getVerbosity() > 2) local_log.concatf("Channel %u is out of range.\n", chan);
-      break;
-  }
-  flushLocalLog();
-}
-
-
-void KridaDimmer::fade_channel(uint8_t chan, bool up) {
-  switch (chan) {
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-      if (nullptr == _channel_timers[chan]) {
-        _channel_timers[chan] = Kernel::returnEvent((up ? MANUVR_MSG_DIMMER_FADE_UP : MANUVR_MSG_DIMMER_FADE_DOWN), this);
-        _channel_timers[chan]->incRefs();
-        _channel_timers[chan]->alterSchedulePeriod(50);
-        _channel_timers[chan]->alterScheduleRecurrence(-1);  // Runs until dimmer boundary.
-        _channel_timers[chan]->autoClear(true);
-        _channel_timers[chan]->enableSchedule(true);
-        _channel_timers[chan]->addArg(chan);
-        platform.kernel()->addSchedule(_channel_timers[chan]);
-      }
-      else {
-        // There is already a timed operation on the channel.
-        if (getVerbosity() > 4) local_log.concatf("KridaDimmer: Won't pulse channel %u because a timer is already operating on the channel.\n", chan);
-      }
-      break;
-
-    default:
-      if (getVerbosity() > 2) local_log.concatf("Channel %u is out of range.\n", chan);
-      break;
-  }
-  flushLocalLog();
-}
-
-
 void KridaDimmer::set_brightness(uint8_t chan, uint8_t nu_brightness) {
   switch (chan) {
     case 0:
--- ManuvrOS/Drivers/KridaDimmer/KridaDimmer.h
+++ ManuvrOS/Drivers/KridaDimmer/KridaDimmer.h
@@ -29,94 +29,40 @@ Driver for the Krida four-channel I2C dimmer.
 #ifndef __KRIDA_4CH_DIMMER_DRIVER_H__
 #define __KRIDA_4CH_DIMMER_DRIVER_H__
 
-#define MANUVR_MSG_DIMMER_SET_LEVEL  0x79B0
-#define MANUVR_MSG_DIMMER_TOGGLE     0x79B1
-#define MANUVR_MSG_DIMMER_FADE_DOWN  0x79B2
-#define MANUVR_MSG_DIMMER_FADE_UP    0x79B3
 
 
-/*
-* Pin defs for this module.
-* Set pin def to 255 to mark it as unused.
-*/
-class KridaDimmerOpts {
+class KridaDimmer : public I2CDevice {
   public:
-    const uint8_t ssr_a_pin;
-    const uint8_t ssr_b_pin;
-    const uint8_t dimmer_addr;
-
-    KridaDimmerOpts(const KridaDimmerOpts* o) :
-      ssr_a_pin(o->ssr_a_pin),
-      ssr_b_pin(o->ssr_b_pin),
-      dimmer_addr(o->dimmer_addr) {};
-
-    KridaDimmerOpts(uint8_t _a_pin, uint8_t _b_pin, uint8_t _daddr) :
-      ssr_a_pin(_a_pin),
-      ssr_b_pin(_b_pin),
-      dimmer_addr(_daddr) {};
-
-
-  private:
-};
-
-
-
-class KridaDimmer : public EventReceiver,
-  #ifdef MANUVR_CONSOLE_SUPPORT
-    public ConsoleInterface,
-  #endif
-    I2CDevice {
-  public:
-    KridaDimmer(const KridaDimmerOpts* opts);
+    KridaDimmer(uint8_t addr);
     ~KridaDimmer();
-
-    /* Overrides from EventReceiver */
-    int8_t notify(ManuvrMsg*);
-    int8_t callback_proc(ManuvrMsg*);
-
-    #ifdef MANUVR_CONSOLE_SUPPORT
-      /* Overrides from ConsoleInterface */
-      uint consoleGetCmds(ConsoleCommand**);
-      inline const char* consoleName() { return "KridaDimmer";  };
-      void consoleCmdProc(StringBuilder* input);
-    #endif  //MANUVR_CONSOLE_SUPPORT
 
     /* Overrides from I2CDevice... */
     int8_t io_op_callahead(BusOp*);
     int8_t io_op_callback(BusOp*);
     void printDebug(StringBuilder*);
 
-    /* Direct channel manipulation. */
-    void enable_channel(uint8_t, bool);
-    bool channel_enabled(uint8_t);
 
-    /* Dimmer breakouts. */
-    void set_brightness(uint8_t, uint8_t);
-    void pulse_channel(uint8_t chan, uint8_t brightness, uint32_t period, int16_t recurrence);
-    void fade_channel(uint8_t chan, bool up);
+    /* Direct channel manipulation. */
+    virtual void enable_channel(uint8_t, bool);
+    virtual bool channel_enabled(uint8_t);
+    virtual void set_brightness(uint8_t, uint8_t);
 
 
   protected:
     int8_t attached();
 
+    bool channel_has_outstanding_io(uint8_t chan);
+    void printChannel(uint8_t chan, StringBuilder*);
+
 
   private:
-    const KridaDimmerOpts _opts;
     uint8_t    _channel_hw[4];      // Last-known value in the hardware.
     uint8_t    _channel_buffer[4];  // I/O operations are conducted against these values.
     uint8_t    _channel_values[4];  // Desired channel values.
     uint8_t    _channel_minima[4];  // Minimum values for each channel.
     uint8_t    _channel_maxima[4];  // Maximum values for each channel.
-    ManuvrMsg* _channel_timers[4];  // Slots for schedules on each channel.
-    bool _ssr_a_enabled = false;
-    bool _ssr_b_enabled = false;
 
-
-    bool channel_has_outstanding_io(uint8_t chan);
-
-    inline bool is_channel_valid(uint8_t chan) {
-      return (chan < 6);
-    };
+    inline bool is_channel_valid(uint8_t chan) {    return (chan < 4);   };
 };
 
 
